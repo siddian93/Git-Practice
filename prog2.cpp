1a6c18c439b0dbecee791bb42624d38e6a5f4d8f
siddian93 <siddian.narayana@gmail.com>
2018-02-24 23:12:47 +0530

    Added a new Function - print_linked_list() -  to Prog2.cpp

--- prog2.cpp
+++ prog2.cpp
@@ -5,3 +5,12 @@ using namespace std;
 int find_min(int a, int b){
     a>b? return a:return b;
 }
+
+void print_linked_list(node* head){
+    node* temp = head;
+    while (temp->next!=\0){
+        cout<<temp->data>>"->";
+        temp = temp->next;
+    }
+    cout<<"[]"<<endl;
+}
