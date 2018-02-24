a9eb85afacd2cec57b37694580888bf8f39180f2
siddian93 <siddian.narayana@gmail.com>
2018-02-24 23:15:18 +0530

    Print Linked List Functionality added to Prog1.cpp

--- prog1.cpp
+++ prog1.cpp
@@ -2,7 +2,14 @@
 
 using namespace std;
 
+struct node{
+    int data;
+    node* next;
+}
+
 int main(){
+    struct node* head;
     cout<<"Hello Dude"<<endl;
+    print_linked_list(head);
     return 0;
 }
